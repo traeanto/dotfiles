4468f05185f2be5d23c906babb8845c4a46b155c
Nilvaela kun <traeanto@gmail.com>
2023-02-28 06:55:12 +0700

    Sweet: New icons

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,10 @@
+# Start X at login
+if status --is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        exec dbus-launch --exit-with-session Hyprland &
+    end
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
